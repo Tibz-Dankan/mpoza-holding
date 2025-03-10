a429e2b6bee9b11171be0b5ee6e2483857495104
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-03-10 20:32:11 +0300

    feat: refine header, add services and footer sections

---
+++ app/components/ServiceCard.tsx
@@ -0,0 +1,29 @@
+import React, { Fragment, ReactNode } from "react";
+
+type ServiceCardProps = React.HTMLAttributes<HTMLDivElement> & {
+  icon: ReactNode;
+  title: string;
+  content: string;
+};
+
+export const ServiceCard: React.FC<ServiceCardProps> = (props) => {
+  return (
+    <Fragment>
+      <div
+        className="bg-white rounded-md border-[1px] border-gray-200 p-6 
+         space-y-4 w-full h-auto shadow-md"
+      >
+        <div className="grid place-items-center items-start">
+          <span
+            className="w-14 h-14 bg-yellow-600 rounded-[50%]
+            grid place-items-center relative"
+          >
+            <span>{props.icon}</span>
+          </span>
+        </div>
+        <p className="text-xl text-gray-800 text-center">{props.title}</p>
+        <p className="text-sm text-gray-600 text-center">{props.content}</p>
+      </div>
+    </Fragment>
+  );
+};
